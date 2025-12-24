89d941ee6f28a0406f95b3807e96a0f0b7cc791f
srinu <nenavath2013@gmail.com>
2025-12-24 10:03:54 +0530

    new-commit

--- provider.tf
+++ provider.tf
@@ -15,5 +15,17 @@ provider "aws" {
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_ca)
-  token                  = data.aws_eks_cluster_auth.this.token
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args = [
+      "eks",
+      "get-token",
+      "--cluster-name",
+      module.eks.cluster_name,
+      "--region",
+      var.region
+    ]
+  }
 }
